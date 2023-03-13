e5c7fc5f1ea82b15e2399c51c9d1bc268a585f4a
dls <dylanlanigansmith@gmail.com>
2023-03-13 18:04:16 -0400

    fixed commiting my API key LOL

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-#OPENAI KEY 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
 import os
 from llama_index import GPTTreeIndex, SimpleDirectoryReader, GPTEmptyIndex
 
@@ -6,24 +5,28 @@ from llama_index import GPTTreeIndex, SimpleDirectoryReader, GPTEmptyIndex
 #ddg(keyword, region="us-en") https://pypi.org/project/duckduckgo-search/
 from duckduckgo_search import ddg
 
-with open('.apikey.txt') as f:
+with open('.apikey.txt') as f: #FILE IN SAME DIRECTORY AS PROGRAM, FIRST LINE IS API KEY NOTHING ELSE
     key = f.readline()
 os.environ['OPENAI_API_KEY'] = key
 f.close()
+if len(key) < 20:
+    print("API KEY IS PROBABLY AN ISSUE") # hashtag never handle exceptions in personal projects 
+    exit()
+
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
 print("skynet 2023")
 def search_augment(query):
     #search_context = " "
-    #"I just asked you to either search or answer the question, 'where do birds go when it rains', you searched, so here are the results of the search from the first web page, please read them and answer my questions briefly, the results are: " + results
+    #from test one: "I just asked you to either search or answer the question, 'where do birds go when it rains', you searched, so here are the results of the search from the first web page, please read them and answer my questions briefly, the results are: " + results
     
     search_idx = query.find("search(")
     if( search_idx != -1):
         search_query = query[query.find("(")+1:query.find(")")] #python be like 
         print("SEARCH AUGMENT! Searching ddg for: " + search_query)
         search_results = ddg(search_query, region="us-en", max_results = 3)
-        url = ducks[0]["href"]
+        url = search_results[0]["href"]
         page = urlopen(url)
         html = page.read().decode("utf-8")
         soup = BeautifulSoup(html, "html.parser")
